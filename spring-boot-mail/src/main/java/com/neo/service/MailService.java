52519ff5619b9aa3233cb269d57e70bcb82d7e0d
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2020-04-18 12:52:07 +0530

    m

--- spring-boot-mail/src/main/java/com/neo/service/MailService.java
+++ spring-boot-mail/src/main/java/com/neo/service/MailService.java
@@ -5,12 +5,12 @@ package com.neo.service;
  */
 public interface MailService {
 
-    void sendSimpleMail(String to, String subject, String content);
+    public void sendSimpleMail(String to, String subject, String content);
 
-    void sendHtmlMail(String to, String subject, String content);
+    public void sendHtmlMail(String to, String subject, String content);
 
-    void sendAttachmentsMail(String to, String subject, String content, String filePath);
+    public void sendAttachmentsMail(String to, String subject, String content, String filePath);
 
-    void sendInlineResourceMail(String to, String subject, String content, String rscPath, String rscId);
+    public void sendInlineResourceMail(String to, String subject, String content, String rscPath, String rscId);
 
 }
--- spring-boot-mail/src/main/java/com/neo/service/impl/MailServiceImpl.java
+++ spring-boot-mail/src/main/java/com/neo/service/impl/MailServiceImpl.java
@@ -85,7 +85,6 @@ public class MailServiceImpl implements MailService{
      * @param content
      * @param filePath
      */
-    @Override
     public void sendAttachmentsMail(String to, String subject, String content, String filePath){
         MimeMessage message = mailSender.createMimeMessage();
 
@@ -117,7 +116,6 @@ public class MailServiceImpl implements MailService{
      * @param rscPath
      * @param rscId
      */
-    @Override
     public void sendInlineResourceMail(String to, String subject, String content, String rscPath, String rscId){
         MimeMessage message = mailSender.createMimeMessage();
 
